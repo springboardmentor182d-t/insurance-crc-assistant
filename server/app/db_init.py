7ac75a26fbc0b9df6cb7c5af6ec23c9fccff935f
Aasritha <aasrithakakumanu24@gmail.com>
2025-12-17 12:36:53 +0530

    Add auth module backend, tests, and gitignore

---
+++ server/app/db_init.py
@@ -0,0 +1,10 @@
+import asyncio
+from app.database import engine, Base
+from app import models  # 🔥 THIS LINE IS REQUIRED
+
+async def init():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+if __name__ == "__main__":
+    asyncio.run(init())
