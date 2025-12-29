a10e5164758e5fbf33c0588e548d78dea45d8091
NandiniAnnareddy <annareddynandini51@gmail.com>
2025-12-29 18:22:31 +0530

    Remove old server/app backend after refactor

--- server/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .profile import Profile
-from .policy import Policy
-from .property_policy import PropertyPolicy
-from .business_policy import BusinessPolicy
--- server/app/models/policy.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class Policy(Base):
-    __tablename__ = "policies"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    premium = Column(Integer)
