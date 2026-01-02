0e858164773cef79a61dc19476da8a51ed9d6997
Vaishu-Sangave <153439689+Vaishu-Sangave@users.noreply.github.com>
2026-01-02 22:37:09 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
-<<<<<<< Updated upstream
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
-import './styles/insurehub.css';
-=======
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -14,10 +13,10 @@ import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
->>>>>>> Stashed changes
+
 
 root.render(
-<<<<<<< Updated upstream
+
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -25,7 +24,7 @@ root.render(
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-=======
+
   <BrowserRouter>
     <App />
   </BrowserRouter>
