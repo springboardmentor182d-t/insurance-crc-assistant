df7de00fceacb4a54f7b6be983e2f931cf765000
NandiniAnnareddy <annareddynandini51@gmail.com>
2026-01-12 19:57:57 +0530

    .

--- server/src/database/core.py
+++ server/src/database/core.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 👉 Change ONLY this line when needed
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/insurance_db"
+#DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/insurance_db"
 # Example for your friend:
-# DATABASE_URL = "postgresql://postgres:Nandini%40163@localhost:5432/insurance_crc"
+DATABASE_URL = "postgresql://postgres:Nandini%40163@localhost:5432/insurance_crc"
 
 engine = create_engine(DATABASE_URL)
 
