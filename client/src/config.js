1524ed217b707e45b8ad308d956d451de9850b1d
Daksh <djain00009@gmail.com>
2026-01-13 14:20:42 +0530

    Revert "Fraud rules intelligence/group c/daksh"

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,3 @@
-process.env.REACT_APP_BASE_URL || "http://localhost:8000";
-=======
+
 export const baseURL =
-  import.meta.env.VITE_BASE_URL || "http://localhost:8000";
+  process.env.REACT_APP_BASE_URL || "http://localhost:8000";
--- server/src/database/core.py
+++ server/src/database/core.py
@@ -1,17 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-#DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/insurance_db"
->>>>>>> origin/main-group-C
-# Example for your friend:
-DATABASE_URL = "postgresql://postgres:Nandini%40163@localhost:5432/insurance_crc"
-=======
 # 👉 Change ONLY this line when needed
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:Nandini%40163@localhost:5432/insurance_crc")
-=======
 #DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/insurance_db"
->>>>>>> origin/main-group-C
 # Example for your friend:
 DATABASE_URL = "postgresql://postgres:Nandini%40163@localhost:5432/insurance_crc"
 
