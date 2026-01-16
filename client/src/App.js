878903c9a548c6966a90a2f35cc9e646c015dd19
NandiniAnnareddy <annareddynandini51@gmail.com>
2026-01-16 20:36:07 +0530

    added frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ProfileProvider } from "./context/ProfileContext";
 import { CompareProvider } from "./context/CompareContext";
 
-/* ===== Auth pages ===== */
+/* ===== AUTH PAGES ===== */
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import Otp from "./pages/Otp";
 import ResetPassword from "./pages/ResetPassword";
-import AdminDashboard from "./pages/AdminDashboard";
+
+/* ===== PROTECTED ROUTE ===== */
 import ProtectedRoute from "./components/ProtectedRoute";
 
-/* ===== Layout & Main pages ===== */
+/* ===== USER LAYOUT & PAGES ===== */
 import PageContainer from "./layout/PageContainer";
 import Home from "./pages/Home";
 import Recommendations from "./pages/Recommendations";
@@ -20,25 +21,7 @@ import ProfilePage from "./pages/ProfilePage";
 import RecommendedPolicies from "./pages/RecommendedPolicies";
 import SavedQuotes from "./pages/SavedQuotes";
 
-/* ===== Recommendation flows ===== */
-import HealthRecommendation from "./pages/HealthRecommendation";
-import MotorRecommendation from "./pages/MotorRecommendation";
-import LifeRecommendation from "./pages/LifeRecommendation";
-import TravelRecommendation from "./pages/TravelRecommendation";
-import HomeRecommendation from "./pages/HomeRecommendation";
-import BusinessRecommendation from "./pages/BusinessRecommendation";
-import FireRecommendation from "./pages/FireRecommendation";
-
-/* ===== Recommendation Results ===== */
-import HealthRecResults from "./pages/HealthRecResults";
-import LifeRecResults from "./pages/LifeRecResults";
-import MotorRecResults from "./pages/MotorRecResults";
-import HomeRecResults from "./pages/HomeRecResults";
-import TravelRecResults from "./pages/TravelRecResults";
-import FireRecResults from "./pages/FireRecResults";
-import BusinessRecResults from "./pages/BusinessRecResults";
-
-/* ===== Claims ===== */
+/* ===== CLAIMS ===== */
 import ClaimsDashboard from "./pages/ClaimsDashboard";
 import StartNewClaim from "./pages/StartNewClaim";
 import FileNewClaimStep1 from "./pages/FileNewClaimStep1";
@@ -48,21 +31,18 @@ import ClaimSubmission from "./pages/ClaimSubmission";
 import ClaimStatus from "./pages/ClaimStatus";
 import TrackClaim from "./pages/TrackClaim";
 
-/* ===== Policy Details ===== */
-import BusinessPolicyDetails from "./pages/BusinessPolicyDetails";
-import LifePolicyDetails from "./pages/LifePolicyDetails";
-import TravelPolicyDetails from "./pages/TravelPolicyDetails";
-import HealthPolicyDetails from "./pages/HealthPolicyDetails";
-import FirePolicyDetails from "./pages/FirePolicyDetails";
-import HomePolicyDetails from "./pages/HomePolicyDetails";
-import MotorPolicyDetails from "./pages/MotorPolicyDetails";
-import PremiumCalculator from "./pages/PremiumCalculator";
-
-/* ===== Compare & Quote ===== */
-import ComparePolicies from "./pages/ComparePolicies";
-import QuoteSummary from "./pages/QuoteSummary";
-
-/* ===== Testing ===== */
+/* ===== ADMIN ===== */
+import AdminLayout from "./layout/AdminLayout";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminProfile from "./pages/AdminProfile";
+import AdminPolicies from "./pages/AdminPolicies";
+import FlaggedClaims from "./pages/FlaggedClaims";
+import FraudRulesEngine from "./pages/FraudRulesEngine";
+import EditFraudRule from "./pages/EditFraudRule";
+import CreateFraudRule from "./pages/CreateFraudRule";
+import InvestigateClaim from "./pages/InvestigateClaim";
+import Investigations from "./pages/Investigations"
+/* ===== TEST ===== */
 import TestFetch from "./pages/TestFetch";
 
 export default function App() {
@@ -72,24 +52,31 @@ export default function App() {
         <BrowserRouter>
           <Routes>
 
-            {/* ===== PUBLIC AUTH ===== */}
+            {/* ================= PUBLIC AUTH ================= */}
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/otp" element={<Otp />} />
             <Route path="/reset-password" element={<ResetPassword />} />
 
-            {/* ===== ADMIN (PROTECTED) ===== */}
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute>
-                  <AdminDashboard />
-                </ProtectedRoute>
-              }
-            />
+            {/* ================= ADMIN (PROTECTED) ================= */}
+            <Route path="/admin" element={<ProtectedRoute><AdminLayout /></ProtectedRoute>}>
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="profile" element={<AdminProfile />} />
+            <Route path="policies" element={<AdminPolicies />} />
 
-            {/* ===== MAIN APP (PROTECTED) ===== */}
+            <Route path="flagged-claims" element={<FlaggedClaims />} />
+            <Route path="flagged-claims/:claimId/investigate" element={<InvestigateClaim />} />
+
+            <Route path="fraud-rules" element={<FraudRulesEngine />} />
+            <Route path="fraud-rules/new" element={<CreateFraudRule />} />
+            <Route path="fraud-rules/:id/edit" element={<EditFraudRule />} />
+            <Route path="investigations" element={<Investigations />} />
+          </Route>
+
+
+            {/* ================= USER APP (PROTECTED) ================= */}
             <Route
               element={
                 <ProtectedRoute>
@@ -102,91 +89,11 @@ export default function App() {
               <Route path="/profile" element={<ProfilePage />} />
               <Route path="/saved-quotes" element={<SavedQuotes />} />
 
-              {/* Recommendations */}
               <Route path="/recommendations" element={<Recommendations />} />
               <Route
                 path="/recommendedPolicies"
                 element={<RecommendedPolicies />}
               />
-
-              {/* Recommendation forms */}
-              <Route
-                path="/health_insurance_rec"
-                element={<HealthRecommendation />}
-              />
-              <Route
-                path="/motor_insurance_rec"
-                element={<MotorRecommendation />}
-              />
-              <Route
-                path="/life_insurance_rec"
-                element={<LifeRecommendation />}
-              />
-              <Route
-                path="/travel_insurance_rec"
-                element={<TravelRecommendation />}
-              />
-              <Route
-                path="/home_insurance_rec"
-                element={<HomeRecommendation />}
-              />
-              <Route
-                path="/business_insurance_rec"
-                element={<BusinessRecommendation />}
-              />
-              <Route
-                path="/fire_property_insurance_rec"
-                element={<FireRecommendation />}
-              />
-
-              {/* Recommendation results */}
-              <Route path="/healthrecresults" element={<HealthRecResults />} />
-              <Route path="/liferecresults" element={<LifeRecResults />} />
-              <Route path="/motorrecresults" element={<MotorRecResults />} />
-              <Route path="/homerecresults" element={<HomeRecResults />} />
-              <Route path="/travelrecresults" element={<TravelRecResults />} />
-              <Route path="/firerecresults" element={<FireRecResults />} />
-              <Route path="/firerecresults" element={<FireRecResults />} />
-              <Route path="/premium-calculator" element={<PremiumCalculator />} />
-
-              <Route
-                path="/businessrecresults"
-                element={<BusinessRecResults />}
-              />
-
-              {/* Policy details */}
-              <Route
-                path="/policies/health/:id"
-                element={<HealthPolicyDetails />}
-              />
-              <Route
-                path="/policies/motor/:id"
-                element={<MotorPolicyDetails />}
-              />
-              <Route
-                path="/policies/life/:id"
-                element={<LifePolicyDetails />}
-              />
-              <Route
-                path="/policies/home/:id"
-                element={<HomePolicyDetails />}
-              />
-              <Route
-                path="/policies/travel/:id"
-                element={<TravelPolicyDetails />}
-              />
-              <Route
-                path="/policies/business/:id"
-                element={<BusinessPolicyDetails />}
-              />
-              <Route
-                path="/policies/fire/:id"
-                element={<FirePolicyDetails />}
-              />
-
-              {/* Compare */}
-              <Route path="/compare" element={<ComparePolicies />} />
-              <Route path="/quote-summary" element={<QuoteSummary />} />
 
               {/* Claims */}
               <Route path="/claims" element={<ClaimsDashboard />} />
@@ -214,7 +121,7 @@ export default function App() {
               <Route path="/test" element={<TestFetch />} />
             </Route>
 
-            {/* ===== FALLBACK ===== */}
+            {/* ================= FALLBACK ================= */}
             <Route path="*" element={<h1>Page Not Found</h1>} />
 
           </Routes>
--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
