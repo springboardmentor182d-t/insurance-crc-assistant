0fbe66a82fca385e04367c117dd67e850d0b1fb8
supriyabadam9-star <supriyabadam9@gmail.com>
2026-01-17 19:02:49 +0530

    added Admindashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import LandingPage from "./pages/Landingpage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
+import AdminDashboard from "./pages/Admindashboard";
+
 
 import PolicyCatalog from "./pages/PolicyCatalog";
 import PolicyDetails from "./pages/PolicyDetails";
@@ -20,6 +22,7 @@ function App() {
         <Route path="/compare" element={<Comparsion />} />
         <Route path="/policies" element={<Policies />} />
         <Route path="/policies/details/:id" element={<PolicyDetails />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />}/>
 
         <Route
           path="*"
