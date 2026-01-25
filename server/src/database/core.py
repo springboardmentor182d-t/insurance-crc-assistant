5a067954758b4c94ee2169c561092d32bd3a63f7
Girish Shirsath <112466858+girishshirsat@users.noreply.github.com>
2026-01-25 23:46:07 +0530

    Refactor database connection setup in core.py

--- server/src/database/core.py
+++ server/src/database/core.py
@@ -4,25 +4,47 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is not set")
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+
+
+
 
 
 engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    connect_args={"sslmode": "require"}
+DATABASE_URL,
+pool_pre_ping=True
 )
 
+
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+autocommit=False,
+autoflush=False,
+bind=engine
 )
+
 
 Base = declarative_base()
 
 
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# register models
 from src.users.models import User
 from src.policies_recommendations_profile_preferences.models.premium_analysis import PremiumAnalysis
 from src.policies_recommendations_profile_preferences.models.recommendation import Recommendation
